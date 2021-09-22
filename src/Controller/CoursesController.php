9d05bda6734dce26f86845214abfbd9a1ead235b
teobryer <teocalme@gmail.com>
2021-09-22 16:15:19 +0200

    first commit

---
+++ src/Controller/CoursesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoursesController extends AbstractController
+{
+    /**
+     * @Route("/", name="courses")
+     */
+    public function recupererListeCourses(ArticleRepository $rep, Request $request): Response
+    {
+        $article = new Article();
+        // associe obj personne au Form.
+        $formArticle = $this->createForm(ArticleType::class,$article);
+        // hydraté $personne en fct du formulaire
+        $formArticle->handleRequest($request);
+        // si le form est validé.
+        if ($formArticle->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $article->setEstAchete(false);
+            $em->persist($article);
+            
+            $em->flush();
+            // je redirig\
+            return $this->redirectToRoute('courses');
+        }
+        $list_courses = $rep->findAll();
+        return $this->render('courses/index.html.twig', [
+            'list_courses' => $list_courses,
+            'formArticle'=> $formArticle->createView()
+        ],
+    );
+    }
+
+    /**
+     * @Route("/delete_article/{id}", name="delete_article")
+     */
+    public function enlever(Article $article, EntityManagerInterface $em): Response
+    {
+       // $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+        return $this->redirectToRoute('courses');
+    }
+
+
+    
+    /**
+     * @Route("/update_article/{id}", name="update_article")
+     */
+    public function rendreAchete(Article $article, EntityManagerInterface $em): Response
+    {
+       // $em = $this->getDoctrine()->getManager();
+        $article->setEstAchete(true);
+        
+        $em->flush();
+        return $this->redirectToRoute('courses');
+    }
+
+
+      
+   
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $estAchete;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEstAchete(): ?bool
+    {
+        return $this->estAchete;
+    }
+
+    public function setEstAchete(bool $estAchete): self
+    {
+        $this->estAchete = $estAchete;
+
+        return $this;
+    }
+}
